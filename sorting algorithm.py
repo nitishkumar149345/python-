fb0c6ffeb8ff9f30b6fa0a7f2d8b1c622d46972f
nitishkumar149345 <149784449+nitishkumar149345@users.noreply.github.com>
2023-11-09 10:47:11 +0530

    Update sorting algorithm.py

--- sorting algorithm.py
+++ sorting algorithm.py
@@ -1,21 +1,21 @@
-list= list(input())
-length = len(list)
+sort_list= list(input())
+length = len(sort_list)
 #bubble sort
 def bubble_sort (a=[]):
     for i in range (length):
         for j in range (i+1,length):
-            if list[i]>list[j]:
-                list[i],list[j]=list[j],list[i]
-bubble_sort(list)
+            if a[i]>a[j]:
+                a[i],a[j]=at[j],a[i]
+bubble_sort(sort_list)
 print ("bubble sorted list  {}".format(list))
 new_list = []
 #selection sort
 def selection_sort(b=[]):
     for i in range (0,length):
-        min_value = min(list)
+        min_value = min(b)
         new_list.append(min_value)
-        list.remove(min_value)
-selection_sort(new_list)
+        b.remove(min_value)
+selection_sort(sort_list)
 
 
 print ("selection sorted list {}".format(new_list))
